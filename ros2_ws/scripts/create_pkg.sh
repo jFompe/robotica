aa6a3dcec782bcbd7bb52a27ad660129adbbfa67
jFompe <jaime.fompe@gmail.com>
2022-10-24 19:06:06 +0200

    Base package script and sources

--- ros2_ws/scripts/create_pkg.sh
+++ ros2_ws/scripts/create_pkg.sh
@@ -5,11 +5,27 @@ PKG_NAME=$1
 [[ -z $PKG_NAME ]] && echo "ERROR: Missing package name arg" && exit 1
 
 
-( \
-cd ../src && \
+PKG_DIR=src/$PKG_NAME
+
+
 ros2 pkg create \
-  --build-type ament_python \
+  --package-format 3 \
   --license MIT \
-  $PKG_NAME && \
-chmod -R 777 $PKG_NAME
-)
+  --destination-directory src/ \
+  --build-type ament_python \
+  --maintainer-email "jaime.fompe@gmail.com" \
+  --maintainer-name "Jaime Fomperosa" \
+  --node-name $PKG_NAME \
+  $PKG_NAME
+
+chmod -R 777 $PKG_DIR
+
+
+mkdir $PKG_DIR/launch
+cp scripts/src/launch.py $PKG_DIR/launch/$PKG_NAME.launch.py
+
+mkdir $PKG_DIR/rviz
+cp scripts/src/urdf_config.rviz $PKG_DIR/rviz/urdf_config.rviz
+
+mkdir $PKG_DIR/models
+cp scripts/src/models/* $PKG_DIR/models/
