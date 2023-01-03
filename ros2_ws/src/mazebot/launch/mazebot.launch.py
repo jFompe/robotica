c89b86afb81782745999e89889abbfef4c8253b3
jFompe <jaime.fompe@gmail.com>
2023-01-03 12:21:23 +0100

    Parameterize use_sim_time in launch files

--- ros2_ws/src/mazebot/launch/mazebot.launch.py
+++ ros2_ws/src/mazebot/launch/mazebot.launch.py
@@ -3,6 +3,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import IncludeLaunchDescription
@@ -16,17 +18,24 @@ def generate_launch_description():
   # Set the path to different files and folders.
   pkg_share = FindPackageShare(package=PKG_NAME).find(PKG_NAME)
   rviz_config_file = os.path.join(pkg_share, 'rviz/rviz_config.rviz')
+
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_sim_time_arg = DeclareLaunchArgument(
+    'use_sim_time',
+    default_value='false',
+    description='Use /clock from Coppelia time if true'
+  )
   
   state = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(PKG_NAME), 'launch', 'state.launch.py'
-                )])
+                )]), launch_arguments={'use_sim_time': use_sim_time}.items()
   )
 
   control = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(PKG_NAME),'launch','control.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control': 'true'}.items()
     )
 
   navigator = IncludeLaunchDescription(
@@ -45,8 +54,9 @@ def generate_launch_description():
   
   # Create the launch description and populate
   return LaunchDescription([
+    use_sim_time_arg,
     start_rviz_cmd,
     state,
-    navigator,
+    # navigator,
     # control
   ])
--- ros2_ws/src/mazebot/launch/state.launch.py
+++ ros2_ws/src/mazebot/launch/state.launch.py
@@ -1,6 +1,8 @@
 import os
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -14,12 +16,19 @@ def generate_launch_description():
   with open(urdf_model_path, 'r') as urdf_f:
     robot_desc = urdf_f.read()
 
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_sim_time_arg = DeclareLaunchArgument(
+    'use_sim_time',
+    default_value='false',
+    description='Use /clock from Coppelia time if true'
+  )
+
   # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
   start_robot_state_publisher_cmd = Node(
     name='robot_state_publisher',
     package='robot_state_publisher',
     executable='robot_state_publisher',
-    parameters=[{'robot_description': robot_desc, 'use_sim_time': True}],
+    parameters=[{'robot_description': robot_desc, 'use_sim_time': use_sim_time}], # must be True or False
     arguments=[urdf_model_path])
 
   # Publish the joint state values for the non-fixed joints in the URDF file.
@@ -28,10 +37,11 @@ def generate_launch_description():
     name='joint_state_publisher',
     package='joint_state_publisher',
     executable='joint_state_publisher',
-    parameters=[{'use_sim_time': False}],
+    parameters=[{'use_sim_time': use_sim_time}],
     arguments=[urdf_model_path])
 
   return LaunchDescription([
+    use_sim_time_arg,
     start_robot_state_publisher_cmd,
     start_joint_state_publisher_cmd
   ])
